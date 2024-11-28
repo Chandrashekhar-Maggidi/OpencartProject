5810ef6d303ba4542b26cf92caa4f7b9173e7b27
Chandrashekhar <chandrashekharmaggidi6@gmail.com>
2024-11-28 11:18:27 +0530

    commit and update testng assertions Print statements

--- opencart/src/test/java/tests/TestHomePage.java
+++ opencart/src/test/java/tests/TestHomePage.java
@@ -6,6 +6,7 @@ import base.BasePage;
 import page.EditAccountPage;
 import page.HomePage;
 import utils.CommonUtils;
+import utils.TestngUtils;
 
 public class TestHomePage extends BasePage {
 	
@@ -25,6 +26,8 @@ public class TestHomePage extends BasePage {
 		CommonUtils.enterValue(editaccountpage.getTelephone(), "12345234", true);
 		CommonUtils.clickElement(editaccountpage.getContinue());
 		CommonUtils.isElementDisplayed(editaccountpage.getSuccess());
+		TestngUtils.assertTrue("Success: Your account has been successfully updated.",CommonUtils.getElementText(editaccountpage.getSuccess()));
+		System.out.println("Account success Notification : " +CommonUtils.getElementText(editaccountpage.getSuccess()));
 	}
 
 
