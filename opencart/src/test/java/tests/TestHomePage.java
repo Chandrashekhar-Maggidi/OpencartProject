835fdf8604cb80513e999a887ccecd137ee2c172
Chandrashekhar <chandrashekharmaggidi6@gmail.com>
2024-11-28 10:30:56 +0530

    commit the edit account info

--- opencart/src/test/java/tests/TestHomePage.java
+++ opencart/src/test/java/tests/TestHomePage.java
@@ -3,17 +3,28 @@ package tests;
 import org.testng.annotations.Test;
 
 import base.BasePage;
+import page.EditAccountPage;
 import page.HomePage;
 import utils.CommonUtils;
 
 public class TestHomePage extends BasePage {
 	
 	HomePage homepage = new HomePage ();
+	EditAccountPage editaccountpage= new EditAccountPage ();
 	
-	@Test
+	@Test(priority=0)
 	public void verifyLoginPage ()
 	{
 		homepage.login();
+	}
+	
+	@Test(priority=1)
+	public void editAccountInformation ()
+	{
+		CommonUtils.clickElement(editaccountpage.getEditaccount());
+		CommonUtils.enterValue(editaccountpage.getTelephone(), "12345234", true);
+		CommonUtils.clickElement(editaccountpage.getContinue());
+		CommonUtils.isElementDisplayed(editaccountpage.getSuccess());
 	}
 
 
